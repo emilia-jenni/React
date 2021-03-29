80bac7b1132fa0d6daf30de13072f69c3f4a15ad
Vuorenmaa Emilia <s2100154@edu.bc.fi>
2021-03-29 16:00:39 +0300

    changed favicon, added styling, README file, header, and footer

--- counter_app/src/App.js
+++ counter_app/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
 
 class App extends Component {
   state = {
@@ -37,10 +39,12 @@ class App extends Component {
 
     return (
       <div>
+        <Header />
         <h1 className={circleClass}>{this.state.counter}</h1>
         <button onClick={this.addHandler}>Add one</button>
         <button onClick={this.removeHandler}>Remove one</button>
         <button onClick={this.resetHandler}>Reset</button>
+        <Footer />
       </div>
     );
   }
---
+++ counter_app/src/README.md
@@ -0,0 +1,16 @@
+# Counter App with React JS
+
+A simple counter where user can add one, remove one, and reset the counter
+This was a school project for learning purposes.
+
+## Screenshot
+
+![Counter App screenshot](counterapp.png)
+
+## Authors and acknowledgment
+
+### Author:
+
+Emilia Vuorenmaa
+
+### GitHub: @emilia_jenni
