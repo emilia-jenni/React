5b4c9c8bb6b271a9ff9147e3e0d4a21e65f402cc
Vuorenmaa Emilia <s2100154@edu.bc.fi>
2021-03-29 14:08:26 +0300

    added circle

--- counter_app/src/App.js
+++ counter_app/src/App.js
@@ -1,11 +1,14 @@
+import React, { Component } from "react";
 import "./App.css";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="circle">0</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
