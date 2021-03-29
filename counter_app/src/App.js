f07dfe6b4f30ae759b703497bd56adecec2e5abb
Vuorenmaa Emilia <s2100154@edu.bc.fi>
2021-03-29 14:46:32 +0300

    selective rendering, different bg colors for even and odd colors

--- counter_app/src/App.js
+++ counter_app/src/App.js
@@ -14,10 +14,9 @@ class App extends Component {
   };
 
   removeHandler = () => {
-    console.log("remove works");
-    this.setState({
-      counter: this.state.counter - 1,
-    });
+    if (this.state.counter !== 0) {
+      this.setState({ counter: this.state.counter - 1 });
+    }
   };
 
   resetHandler = () => {
@@ -28,9 +27,17 @@ class App extends Component {
   };
 
   render() {
+    let circleClass = `${
+      this.state.counter === 0
+        ? ""
+        : this.state.counter % 2 === 0
+        ? "even"
+        : "odd"
+    } circle`;
+
     return (
       <div>
-        <h1 className="circle">{this.state.counter}</h1>
+        <h1 className={circleClass}>{this.state.counter}</h1>
         <button onClick={this.addHandler}>Add one</button>
         <button onClick={this.removeHandler}>Remove one</button>
         <button onClick={this.resetHandler}>Reset</button>
