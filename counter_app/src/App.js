56f21841c5697e4a16c04ca481bca055f0f48035
Vuorenmaa Emilia <s2100154@edu.bc.fi>
2021-03-29 14:19:49 +0300

    added buttons add, remove, reset

--- counter_app/src/App.js
+++ counter_app/src/App.js
@@ -2,10 +2,38 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    counter: 0,
+  };
+
+  addHandler = () => {
+    console.log("adding works");
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+
+  removeHandler = () => {
+    console.log("remove works");
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+  };
+
+  resetHandler = () => {
+    console.log("reset works");
+    this.setState({
+      counter: 0,
+    });
+  };
+
   render() {
     return (
       <div>
-        <h1 className="circle">0</h1>
+        <h1 className="circle">{this.state.counter}</h1>
+        <button onClick={this.addHandler}>Add one</button>
+        <button onClick={this.removeHandler}>Remove one</button>
+        <button onClick={this.resetHandler}>Reset</button>
       </div>
     );
   }
